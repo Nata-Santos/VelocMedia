11709a1422b87569346d1098770a3c45d621cce5
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-03-25 23:53:02 -0300

    entrega VelocMedia

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, s ;
+           int v;
+
+           Console.WriteLine("Digite a distância andada, e o tempo gasto para chegar ao destino.");
+           Console.WriteLine("---------------------------");
+
+           Console.WriteLine("Distância percorrida (m): ");
+           m = Convert.ToInt16(Console.ReadLine());
+
+           Console.WriteLine("Tempo gasto (s): ");
+           s = Convert.ToInt16(Console.ReadLine());
+
+           v = m/s ;
+           Console.WriteLine($"\nA velocidade média é ►► {v} m/s ");
+
+        }
+    }
+}
